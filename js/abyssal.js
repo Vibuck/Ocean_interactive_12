0c50cd917f30f1b97dbf0d1c35d4891abd3fd7e9
tunggg1 <24023087@vnu.edu.vn>
2026-04-29 19:09:15 +0700

    1

--- js/abyssal.js
+++ js/abyssal.js
@@ -1,30 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger);
 
-    const tl = gsap.timeline({
+    // TRIGGER 1: CUỘN NGANG 3 ẢNH THÔNG TIN
+    gsap.to(".info-horizontal-wrapper", {
+        xPercent: -66.66, // Dịch chuyển để hiện ảnh 2 và 3 (100% * (3-1)/3)
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#abyssal-info-scroll",
+            start: "top top",
+            end: "+=200%", // Độ dài cuộn chuột cho 3 ảnh
+            pin: true,     // Ghim lại cho đến khi xem hết 3 ảnh
+            scrub: 1,
+            snap: 1 / 2    // Tự động dừng đúng từng ảnh
+        }
+    });
+
+    // TRIGGER 2: HIỆU ỨNG 3 CON CÁ BUNG RA (CODE CŨ CỦA BẠN)
+    const tlFish = gsap.timeline({
         scrollTrigger: {
             trigger: "#abyssal-fish-animation",
             start: "top top",
-            end: "+=100%", 
+            end: "+=100%",
             scrub: 1,
             pin: true,
         }
     });
 
-    // Tách 3 ảnh ra các vị trí cân đối và DỪNG LẠI (không ẩn đi)
-    tl.to(".fish-left", { 
-        xPercent: -150, // Đẩy sang trái 1.5 lần kích thước ảnh
+    tlFish.to(".fish-left", { 
+        xPercent: -150, 
         rotation: -10, 
         duration: 2 
     })
     .to(".fish-right", { 
-        xPercent: 150,  // Đẩy sang phải 1.5 lần kích thước ảnh
+        xPercent: 150,  
         rotation: 10, 
         duration: 2 
     }, 0)
     .to(".fish-center", { 
-        yPercent: 0,    // Giữ nguyên vị trí Y hoặc điều chỉnh nhẹ nếu muốn
-        scale: 1.1,     // Phóng to nhẹ ảnh giữa để làm điểm nhấn
+        scale: 1.1,     
         duration: 2 
     }, 0);
 });
