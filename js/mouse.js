7e8f693b975ba9a9c76c6612dc77c0a5468f41af
Vibuck <24023095@vnu.edu.vn>
2026-05-04 10:00:32 +0700

    mouse update

---
+++ js/mouse.js
@@ -0,0 +1,34 @@
+const coords ={x:0,y:0};
+const circles = document.querySelectorAll(".circle");
+
+circles.forEach(function(circle){
+    circle.x=0;
+    circle.y=0;
+});
+
+window.addEventListener("mousemovement", function(e){
+    coords.x=e.clientX;
+    coords.y=e.clientY;
+    
+});
+
+function animateCircles(){
+    let x = coords.x;
+    let y = coords.y;
+
+    circles.forEach(function(circle, index){
+        circle.style.left = x - 12 + "px";
+        circle.style.top = x - 12 + "px";
+        circle.style.scale =(circles.length-index)/ circles.length;
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circle [index+1];
+        x+=(nextCircle.x-x)*0.3;
+        y+=(nextCircle.y-y)*0.3;
+    });
+    requestAnimationFrame(animateCircles);
+}
+animateCircles();
+
+
